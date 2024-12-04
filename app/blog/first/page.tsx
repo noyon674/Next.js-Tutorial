398acfbe7189d1e3cb8d07cdea437e6939f43ff3
noyon674 <developernoyon9@gmail.com>
2024-12-04 14:14:27 +0600

    added nested route

---
+++ app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>First blog page</div>
+  )
+}
+
+export default page
---
+++ app/blog/second/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Second blog page</div>
+  )
+}
+
+export default page
